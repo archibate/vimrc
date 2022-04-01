557297afd95c7c642204ab82363e818b77de69f9
archibate <1931127624@qq.com>
2022-04-01 16:27:28 +0800

    upda

--- release/install.sh
+++ release/install.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-
 set -e
+echo '-- Welcome to Archibate Vimrc installation script :)'
 
 cd "$(dirname $0)"/..
 test -d ./.vim
+
 
 get_linux_distro() {
     if grep -Eq "Ubuntu" /etc/*-release; then
@@ -44,37 +45,50 @@ get_linux_distro() {
 }
 
 
-install_pacman() {
-    pacman -S --noconfirm fzf ripgrep
-    pacman -S --noconfirm nodejs
-    pacman -S --noconfirm ccls
+install_coc_plugins() {
+    for x in coc-ccls coc-pyright coc-json coc-git; do
+        echo "-- Installing coc plugin '$x', please wait..."
+        vim --not-a-term -c "CocInstall -sync $x | quit"
+    done
+
     mkdir -p ~/.config/coc/extensions/node_modules/coc-ccls
     ln -sf node_modules/ws/lib ~/.config/coc/extensions/node_modules/coc-ccls/lib
 }
 
 
-install_apt() {
-    DEBIAN_FRONTEND=noninteractive apt-get install -y fzf ripgrep
-    DEBIAN_FRONTEND=noninteractive apt-get install -y clang libclang-dev
-    DEBIAN_FRONTEND=noninteractive apt-get install -y cmake make
+install_pacman() {
+    sudo pacman -S --noconfirm fzf ripgrep
+    sudo pacman -S --noconfirm nodejs
+    sudo pacman -S --noconfirm ccls
 
-    if [ `node --version | sed s/v// | cut -f1 -d.` -lt 12 ]; then
-        bash .vim/ccls/setup_12.x
-        DEBIAN_FRONTEND=noninteractive apt-get install -y nodejs
+    install_coc_plugins
+}
+
+
+install_ccls_from_source() {
+    cd .vim/ccls
+    rm -rf /tmp/ccls-build.$$
+    cmake -B /tmp/ccls-build.$$ -DCMAKE_BUILD_TYPE=Release
+    cmake --build /tmp/ccls-build.$$ --config Release --parallel 4
+    cmake --build /tmp/ccls-build.$$ --config Release --target install
+    rm -rf /tmp/ccls-build.$$
+    cd ../..
+}
+
+
+install_apt() {
+    sudo apt-get install -y fzf ripgrep
+    sudo apt-get install -y clang libclang-dev
+    sudo apt-get install -y cmake make g++
+
+    if ! which node || [ `node --version | sed s/v// | cut -f1 -d.` -lt 12 ]; then
+        echo '-- Upgrading Node.js to 12.x'
+        curl -sL install-node.vercel.app/lts | bash -s - --force --prefix /usr
+        node --version
     fi
 
-    cd .vim/ccls
-    rm -rf /tmp/ccls-build
-    cmake -B /tmp/ccls-build -DCMAKE_BUILD_TYPE=Release
-    cmake --build /tmp/ccls-build --config Release --parallel 4
-    cmake --build /tmp/ccls-build --config Release --target install
-    rm -rf /tmp/ccls-build
-    cd ../..
-
-    vim -c 'CocInstall coc-ccls | CocInstall coc-pyright | CocInstall coc-json | CocInstall coc-git | quit'
-
-    mkdir -p ~/.config/coc/extensions/node_modules/coc-ccls
-    ln -sf node_modules/ws/lib ~/.config/coc/extensions/node_modules/coc-ccls/lib
+    install_ccls_from_source
+    install_coc_plugins
 }
 
 
@@ -92,10 +106,16 @@ elif [ $distro == "ManjaroLinux" ]; then
 else
     echo "-- Unsupported distro: $distro"
     echo "-- Please manually install: fzf ripgrep nodejs ccls"
+    echo "-- If you know how to install them, feel free to contribute to this GitHub repository: github.com/archibate/vimrc"
+    echo "-- Hint: Node.js 12.x and above is required. Try the following patch script if you meet issues about coc.nvim:"
+    cat <<EOF
+mkdir -p ~/.config/coc/extensions/node_modules/coc-ccls
+ln -sf node_modules/ws/lib ~/.config/coc/extensions/node_modules/coc-ccls/lib
+EOF
 fi
 
-if [ "x$PWD" == "x$HOME" ]; then
-    [ -f ~/.vimrc ] || mv ~/.vimrc /tmp/backup.$$.vimrc
-    [ -f ~/.vim ] || mv ~/.vim /tmp/backup.$$.vim
+if [ "x$PWD" != "x$HOME" ]; then
+    [ -f ~/.vimrc ] && mv ~/.vimrc /tmp/backup.$$.vimrc
+    [ -d ~/.vim ] && mv ~/.vim /tmp/backup.$$.vim
     cp -r .vimrc .vim ~/
 fi
