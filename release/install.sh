83e3e1983fe7aa5fa7d0272b8b2fd1f7005de77b
archibate <1931127624@qq.com>
2022-04-02 13:51:34 +0800

    sudo warn

--- release/install.sh
+++ release/install.sh
@@ -95,9 +95,17 @@ install_ccls_from_source() {
 
 install_nodejs_lts() {
     if ! which node || [ `node --version | sed s/v// | cut -f1 -d.` -lt 12 ]; then
-        echo '-- Upgrading Node.js to 12.x'
-        sudo bash -c 'curl -sL install-node.vercel.app/lts | bash -s - --force --prefix /usr'
+        echo '-- Upgrading Node.js version to 12.x'
+        if [ "x$FORCE" != "xy" ]; then
+            sudo bash -c 'curl -sL install-node.vercel.app/lts | bash -s - --force --prefix /usr'
+        else
+            sudo bash -c 'curl -sL install-node.vercel.app/lts | bash -s - --prefix /usr'
+        fi
         node --version
+        if [ "x$FORCE" != "xy" ]; then
+            echo -n "-- Would you like to switch the npm source to aliyun now? (y/N)"; read -n1 x; echo
+            if [ "x$x" == "xy" ]; then echo "OK, press Ctrl-D when you done..."; bash; fi
+        fi
     fi
 }
 
