f6da0c12502d730cb97b02d18b731df4b2ade641
archibate <1931127624@qq.com>
2022-04-12 15:16:10 +0800

    shift-f2/f3

--- lua/archvim/config/bufferline.lua
+++ lua/archvim/config/bufferline.lua
@@ -1,6 +1,5 @@
 require'bufferline'.setup {
     options = {
-        diagnostics = "nvim_lsp",
         offsets = {{
             filetype = "NvimTree",
             text = "File Explorer",
@@ -10,9 +9,22 @@ require'bufferline'.setup {
         close_command = function(bufnum)
             require('bufdelete').bufdelete(bufnum, true)
         end,
+        diagnostics = "nvim_lsp",
+        diagnostics_indicator = function(count, level, diagnostics_dict, context)
+            return "("..count..")"
+        end,
+        sort_by = 'insert_after_current',
+        -- custom_filter = function(buf_number, buf_numbers)
+        --     -- filter out filetypes you don't want to see
+        --     if vim.bo[buf_number].filetype ~= "toggleterm" then
+        --         return true
+        --     end
+        -- end,
     }
 }
 
 local map = require'archvim/mappings'
 map("n", "<F2>", "<cmd>BufferLineCyclePrev<CR>", { silent = true })
 map("n", "<F3>", "<cmd>BufferLineCycleNext<CR>", { silent = true })
+map("n", "<F14>", "<cmd>BufferLineMovePrev<CR>", { silent = true })
+map("n", "<F15>", "<cmd>BufferLineMoveNext<CR>", { silent = true })
