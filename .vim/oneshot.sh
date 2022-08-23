3595c3e6f997dedcc2c58f467dcdbd4772d082eb
archibate <1931127624@qq.com>
2022-08-23 14:46:37 +0800

    payloadxz

--- .vim/oneshot.sh
+++ .vim/oneshot.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# https://stackoverflow.com/questions/29418050/package-tar-gz-into-a-shell-script
 set -e
 
 cd "`dirname "${0}"`/.."
@@ -10,13 +9,14 @@ tmp=/tmp/__extract__$RANDOM
 
 .vim/package.sh
 
+# https://stackoverflow.com/questions/29418050/package-tar-gz-into-a-shell-script
 printf "#!/bin/bash
 set -e
 PAYLOAD_LINE=\`awk '/^__PAYLOAD_BELOW__/ {print NR + 1; exit 0; }' \$0\`
 rm -rf /tmp/_extract_.\$\$
 mkdir -p /tmp/_extract_.\$\$
 cd /tmp/_extract_.\$\$
-tail -n+\$PAYLOAD_LINE \$0 | tar -xvz
+tail -n+\$PAYLOAD_LINE \$0 | tar -xz
 .vim/install.sh
 rm -rf /tmp/_extract_.\$\$
 
