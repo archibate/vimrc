81ab8ad0ffeeb6e5a0e38eb48724c06c969e124b
archibate <1931127624@qq.com>
2023-09-27 13:55:15 +0800

    o

--- lua/archvim/config/telescope.lua
+++ lua/archvim/config/telescope.lua
@@ -11,31 +11,31 @@ require("telescope").setup{
 
 local map = require'archvim/mappings'
 -- 查找文件
-map("n", "<leader>u", "<cmd>Telescope find_files initial_mode=insert<CR>")
+map("n", "<leader>l", "<cmd>Telescope find_files initial_mode=insert<CR>")
 -- 查找 git 仓库中的文件
 map("n", "<leader>i", "<cmd>Telescope git_files initial_mode=insert<CR>")
 -- 查找最近打开过的文件
-map("n", "<leader>o", "<cmd>Telescope oldfiles<CR>")
+map("n", "<leader>o", "<cmd>Telescope oldfiles initial_mode=insert<CR>")
 -- 查找 git status 中的文件
-map("n", "<leader>p", "<cmd>Telescope git_status<CR>")
+map("n", "<leader>p", "<cmd>Telescope git_status initial_mode=insert<CR>")
 -- 查找当前项目文件中的文字
 map("n", "<leader>k", "<cmd>Telescope live_grep initial_mode=insert<CR>")
 -- 查找所有已打开文件
-map("n", "<leader>l", "<cmd>Telescope buffers<CR>")
+map("n", "<leader>,", "<cmd>Telescope buffers initial_mode=insert<CR>")
 -- 查找 vim 的跳转记录
-map("n", "<leader>j", "<cmd>Telescope jumplist<CR>")
+map("n", "<leader>j", "<cmd>Telescope jumplist initial_mode=insert<CR>")
 -- 查找 vim 的标记
-map("n", "<leader>m", "<cmd>Telescope marks<CR>")
+map("n", "<leader>m", "<cmd>Telescope marks initial_mode=insert<CR>")
 -- 查找 / 的搜索记录
-map("n", "<leader>/", "<cmd>Telescope search_history<CR>")
+map("n", "<leader>/", "<cmd>Telescope search_history initial_mode=insert<CR>")
 -- 查找 vim 命令历史记录
-map("n", "<leader>:", "<cmd>Telescope command_history<CR>")
+map("n", "<leader>:", "<cmd>Telescope command_history initial_mode=insert<CR>")
 -- 查找所有 vim 命令
-map("n", "<leader>;", "<cmd>Telescope commands<CR>")
+map("n", "<leader>;", "<cmd>Telescope commands initial_mode=insert<CR>")
 -- 查找帮助文档
 map("n", "<leader>?", "<cmd>Telescope help_tags initial_mode=insert<CR>")
 -- 查找 todo 等事项
-map("n", "<leader>t", "<cmd>TodoTelescope<CR>")
+map("n", "<leader>t", "<cmd>TodoTelescope initial_mode=insert<CR>")
 -- 查找 git 仓库的 commit 历史
 map("n", "<leader>c", "<cmd>Telescope git_commits<CR>")
 -- 查找 git 仓库的所有分支
@@ -45,6 +45,8 @@ map("n", "<leader>a", "<cmd>Telescope diagnostics<CR>")
 
 -- 查找符号定义（LSP）
 map("n", "gd", "<cmd>Telescope lsp_definitions<CR>")
+-- 查找类型定义（LSP）
+map("n", "gsd", "<cmd>Telescope lsp_type_definitions<CR>")
 -- 查找所有引用（LSP）
 map("n", "gr", "<cmd>Telescope lsp_references<CR>")
 -- 查找函数实现（LSP）
