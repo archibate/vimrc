f588bf521a00c74f0758f0a19260a64d0754dcf5
Quartz <yyhran@163.com>
2025-05-16 11:28:20 +0800

    modify linux-x86_64 nvim.appimage name
    Signed-off-by: Quartz <yyhran@163.com>

--- scripts/install_nvim.sh
+++ scripts/install_nvim.sh
@@ -34,7 +34,7 @@ if [ "x$(uname -sm)" = "xLinux x86_64" ]; then
     if which snap >/dev/null 2>&1; then
         sudo snap remove nvim || true
     fi
-    test -f ./nvim.appimage || curl -L https://github.com/neovim/neovim/releases/latest/download/nvim.appimage -o ~/.config/nvim/nvim.appimage
+    test -f ./nvim.appimage || curl -L https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.appimage -o ~/.config/nvim/nvim.appimage
     sudo chmod +x ./nvim.appimage
     test -f /usr/bin/nvim && sudo mv /usr/bin/nvim /tmp/.nvim-executable-backup || true
     sudo cp ./nvim.appimage /usr/bin/nvim
